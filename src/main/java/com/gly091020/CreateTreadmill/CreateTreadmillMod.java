76b7802e8f4f43d8cb8650a44735718d69577076
gly091020 <gulangyu1020@qq.com>
2025-08-17 18:13:40 +0800

    隐藏成就

--- src/main/java/com/gly091020/CreateTreadmill/CreateTreadmillMod.java
+++ src/main/java/com/gly091020/CreateTreadmill/CreateTreadmillMod.java
@@ -4,7 +4,6 @@ import com.gly091020.CreateTreadmill.block.TreadmillBlock;
 import com.gly091020.CreateTreadmill.block.TreadmillBlockEntity;
 import com.gly091020.CreateTreadmill.item.TreadmillItem;
 import com.gly091020.CreateTreadmill.maid.MaidPlugin;
-import com.gly091020.CreateTreadmill.ponder.TreadmillPonderPlugin;
 import com.gly091020.CreateTreadmill.renderer.TreadmillRenderer;
 import com.gly091020.CreateTreadmill.renderer.TreadmillVisual;
 import com.mojang.logging.LogUtils;
@@ -17,9 +16,12 @@ import com.tterrag.registrate.util.entry.ItemEntry;
 import dev.engine_room.flywheel.lib.model.baked.PartialModel;
 import net.createmod.catnip.render.SpriteShiftEntry;
 import net.createmod.catnip.render.SpriteShifter;
-import net.createmod.ponder.foundation.PonderIndex;
+import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.ServerAdvancementManager;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.IEventBus;
@@ -28,6 +30,7 @@ import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RenderLivingEvent;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import org.slf4j.Logger;
 
 import java.util.HashMap;
@@ -90,6 +93,26 @@ public class CreateTreadmillMod {
                     speed = (int) (Math.abs(entity.getSpeed()) / 32);
                 }
                 event.getEntity().walkAnimation.setSpeed(speed);
+            }
+        }
+
+        @SubscribeEvent
+        public static void onEntityDie(LivingDeathEvent deathEvent){
+            var entity = deathEvent.getEntity();
+            if(entity.level().getBlockState(entity.blockPosition()).is(TREADMILL_BLOCK)){
+                var last = entity.getLastAttacker();
+                if(last instanceof ServerPlayer player){
+                    grantAdvancement(player, ResourceLocation.fromNamespaceAndPath(ModID, "run_to_die"), "0");
+                }
+            }
+        }
+
+        public static void grantAdvancement(ServerPlayer player, ResourceLocation advancementId, String key) {
+            ServerAdvancementManager manager = player.server.getAdvancements();
+            AdvancementHolder advancement = manager.get(advancementId);
+
+            if (advancement != null) {
+                player.getAdvancements().award(advancement, key);
             }
         }
     }
